a611f252499fba3a8fd0a78246964603dc17cdb7
Kevin Longeway <KevinLongeway@gmail.com>
2025-11-10 15:26:00 -0700

    It now handles other books in the directory

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from stats import get_num_words, character_frequency, sorted_list
 count_characters = character_frequency
@@ -12,13 +13,18 @@ def get_book_text(filepath: str) -> str:
     return path.read_text(encoding="utf-8")
 
 def main():
+    if sys.argv[1:] == []:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     try:
-        text = get_book_text("books/frankenstein.txt")
+        text = get_book_text(book_path)
     except Exception as e:
         print(e)
         return
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {get_num_words(text)} total words")
     print("--------- Character Count -------")
